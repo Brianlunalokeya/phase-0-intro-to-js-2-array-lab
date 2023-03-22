a61308a3de2f5b35092c4a52acb41c026f251a75
+ <+>
2023-03-22 08:45:07 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
-// Write your solution here!
-// create an initial array of cats
-const cats = ["Milo", "Otis", "Garfield"];
+let cats = ["Milo", "Otis", "Garfield"];
 
-// append a cat to the end of the array
-function destructivelyAppendCat(name) {
-cats.push(name);
+function destructivelyAppendCat() {
+  return cats.push("Ralph");
 }
 
-// prepend a cat to the beginning of the array
-function destructivelyPrependCat(name) {
-cats.unshift(name);
+function destructivelyPrependCat() {
+  return cats.unshift("Bob");
 }
 
-// remove the last cat from the array
 function destructivelyRemoveLastCat() {
-cats.pop();
+  return cats.pop();
 }
 
-// remove the first cat from the array
 function destructivelyRemoveFirstCat() {
-cats.shift();
+  return cats.shift();
 }
 
-// append a cat to the end of the array and return a new array
-function appendCat(name) {
-const newCats = [...cats, name];
-return newCats;
+function appendCat() {
+  let newCats = [...cats, "Broom"];
+  return newCats;
 }
 
-// prepend a cat to the beginning of the array and return a new array
-function prependCat(name) {
-const newCats = [name, ...cats];
-return newCats;
+function prependCat() {
+  let newCats = ["Arnold", ...cats];
+  return newCats;
 }
 
-// remove the last cat from the array and return a new array
 function removeLastCat() {
-const newCats = cats.slice(0, -1);
-return newCats;
+  let newCats = cats.slice(0, -1);
+  return newCats;
 }
 
-// remove the first cat from the array and return a new array
 function removeFirstCat() {
-const newCats = cats.slice(1);
-return newCats;
+  let newCats = cats.slice(1);
+  return newCats;
 }
